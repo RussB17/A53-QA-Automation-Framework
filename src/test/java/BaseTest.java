1767ae9e8c3e8597f54774ba03145845606a6458
RussB17 <ruslan.bohonosyuk@testpro.io>
2024-01-15 13:59:00 -0700

    how to find successful message locator

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,6 +1,13 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import java.time.Duration;
 
 public class BaseTest {
 
@@ -15,19 +22,20 @@ public class BaseTest {
 
     @BeforeMethod
     public void launchBrowser(){
-        //Chrome Options
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-        //Manage Browser - wait for 10 seconds before failing/quitting.
+
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
-        //Navigate to Url
+
         navigateToUrl();
     }
 
     @AfterMethod
     public void closeBrowser(){
+
         driver.quit();
     }
 
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,43 +1,28 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
+
 public class LoginTests extends BaseTest {
+
+
+
 //    @Test
-//    public void loginEmptyEmailPassword() {
-//
-////      Added ChromeOptions argument below to fix websocket error
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--remote-allow-origins=*");
-//
-//        WebDriver driver = new ChromeDriver(options);
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//
-//        String url = "https://qa.koel.app/";
-//        driver.get(url);
+//    public void navigateToKoelApp() {
+//        navigateToUrl();
 //        Assert.assertEquals(driver.getCurrentUrl(), url);
 //        driver.quit();
 //    }
-//}
-
-    @Test
-    public void navigateToKoelApp() {
-        navigateToUrl();
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
-    }
 
     @Test
     public void loginValidEmailPassword() {
         try {
-            provideEmail("demo@class.com");
-            providePassword("te$t$tudent");
+            provideEmail("rusboh@testpro.io");
+            providePassword("Te$t$tudent");
             clickSubmit();
-            //Assertion
             WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
             Assert.assertTrue(avatarIcon.isDisplayed());
         } catch (Exception e) {
@@ -45,31 +30,30 @@ public class LoginTests extends BaseTest {
         }
     }
 
-    @Test
-    public void loginWithInvalidEmailValidPassword() throws InterruptedException {
-        provideEmail("invalid@class.com");
-        //Password Field
-        providePassword("te$t$tudent");
-        //Submit button
-        clickSubmit();
-        Thread.sleep(2000);
-        //Expected Result
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-    }
-
-    @Test
-    public void loginWithInvalidPasswordAndValidEmail() throws InterruptedException {
-        //navigateToUrl();
-        //Email Field
-        provideEmail("demo@class.com");
-        //Password Field
-        providePassword("invalidPassword");
-        //Submit button
-        clickSubmit();
-        Thread.sleep(2000);
-        //Expected Result
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-    }
+//    @Test
+//    public void loginWithInvalidEmailValidPassword() throws InterruptedException {
+//        provideEmail("invalid@class.com");
+//
+//        providePassword("te$t$tudent");
+//
+//        clickSubmit();
+//        Thread.sleep(2000);
+//
+//        Assert.assertEquals(driver.getCurrentUrl(), url);
+//    }
+//
+//    @Test
+//    public void loginWithInvalidPasswordAndValidEmail() throws InterruptedException {
+//
+//        provideEmail("demo@class.com");
+//
+//        providePassword("invalidPassword");
+//
+//        clickSubmit();
+//        Thread.sleep(2000);
+//
+//        Assert.assertEquals(driver.getCurrentUrl(), url);
+//    }
 
 
 }
